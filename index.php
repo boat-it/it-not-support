d5551b7a65330e18686435380f240de4f3258d16
boat-it <55787836+boat-it@users.noreply.github.com>
2019-09-29 16:56:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,53 @@
 <?php
-print "HELLO TEST";
-print date("now");
+// URL API LINE
+$API_URL = 'https://api.line.me/v2/bot/message';
+// ใส่ Channel access token (long-lived)
+$ACCESS_TOKEN = 'kT9H2mrXWPMGeaTwwUpqu3RXRTTghlSAHXaPk+jZWC7kW8lI9pkbi8po6wemhLv3wzp7FUnh52sTOYbu+b1pPWMTIkGuqEuKAG2h3oqHFtkc23sSukoDHo6+o2e64a01J00m0JVo4h4wM2jDD+r2bQdB04t89/1O/w1cDnyilFU=';
+// ใส่ Channel Secret
+$CHANNEL_SECRET = 'a1f533a764bf17c9fa17d217ba1efe55';
+
+
+// Set HEADER
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);// Get request content
+$request = file_get_contents('php://input');// Decode JSON to Array
+$request_array = json_decode($request, true);
+
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+if ( sizeof($request_array['events']) > 0 ) {
+    foreach ($request_array['events'] as $event) {
+    
+    $reply_message = '';
+    $reply_token = $event['replyToken'];
+    $data = [
+       'replyToken' => $reply_token,
+       'messages' => [
+          ['type' => 'text', 
+           'text' => json_encode($request_array)]
+       ]
+    ];
+    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+    $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);      echo "Result: ".$send_result."\r\n";   }
+}
+echo "OK";
+
+
+
+
+
+
+
+
 ?>
