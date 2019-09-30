69bc5b264c74f7eaa8e442c1852c834ff0e665fc
boat-it <55787836+boat-it@users.noreply.github.com>
2019-09-30 12:02:52 +0700

    Update connectstring.php

--- connectstring.php
+++ connectstring.php
@@ -1,11 +1,14 @@
 <?php
 $db = parse_url(getenv("postgres://ptplxvkmvcsjez:27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872@ec2-23-21-94-99.compute-1.amazonaws.com:5432/d35djr9rl60o00"));
-
 try{
-$dbh = new PDO('pgsql:dbname=ec2-23-21-94-99.compute-1.amazonaws.com;
-                           host=5432;
-                           username=ptplxvkmvcsjez;
-                           password=27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872');
+          $dbname="d35djr9rl60o00";
+          $host="5432";
+          $username="ptplxvkmvcsjez";
+          $passwd="27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872";
+$dbh = new PDO("pgsql:dbname=$dbname;
+                           host=$host;
+                           username=$username;
+                           password=$passwd");
 // $pdo = new PDO("pgsql:" . sprintf(
 //     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
 //     $db["ec2-23-21-94-99.compute-1.amazonaws.com"],
