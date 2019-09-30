b8c9af944c3395256235882596d94f82b1b993cb
boat-it <55787836+boat-it@users.noreply.github.com>
2019-09-30 12:05:23 +0700

    Update connectstring.php

--- connectstring.php
+++ connectstring.php
@@ -5,10 +5,10 @@ try{
           $host="5432";
           $username="ptplxvkmvcsjez";
           $passwd="27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872";
-$dbh = new PDO("pgsql:dbname=$dbname;
+$dbh = new PDO('pgsql:dbname=$dbname;
                            host=$host;
                            username=$username;
-                           password=$passwd");
+                           password=$passwd');
 // $pdo = new PDO("pgsql:" . sprintf(
 //     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
 //     $db["ec2-23-21-94-99.compute-1.amazonaws.com"],
