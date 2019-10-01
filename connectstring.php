9527d8e172ba11089d2200ba5e33434ed01af1df
boat-it <55787836+boat-it@users.noreply.github.com>
2019-10-01 16:39:35 +0700

    Update connectstring.php

--- connectstring.php
+++ connectstring.php
@@ -2,21 +2,11 @@
 $db = parse_url(getenv("postgres://ptplxvkmvcsjez:27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872@ec2-23-21-94-99.compute-1.amazonaws.com:5432/d35djr9rl60o00"));
 try{
           $dbname="d35djr9rl60o00";
-          $host="5432";
+          $host="ec2-23-21-94-99.compute-1.amazonaws.com";
           $username="ptplxvkmvcsjez";
           $passwd="27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872";
-$dbh = new PDO('pgsql:dbname=$dbname;
-                           host=$host;
-                           username=$username;
-                           password=$passwd');
-// $pdo = new PDO("pgsql:" . sprintf(
-//     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-//     $db["ec2-23-21-94-99.compute-1.amazonaws.com"],
-//     $db["5432"],
-//     $db["ptplxvkmvcsjez"],
-//     $db["27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872"],
-//     ltrim($db["d35djr9rl60o00"], "/")
-// ));
+$dbh = new PDO("pgsql:dbname=$dbname;host=$host;username=$username;password=$passwd");
+
 }catch(Exception $e){
 echo $e->getMessage();
 }
