3d804d7f806957005f62bc5889a0e31bcdad90ec
boat-it <55787836+boat-it@users.noreply.github.com>
2019-10-01 16:42:26 +0700

    Update connectstring.php

--- connectstring.php
+++ connectstring.php
@@ -5,7 +5,7 @@ try{
           $host="ec2-23-21-94-99.compute-1.amazonaws.com";
           $username="ptplxvkmvcsjez";
           $passwd="27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872";
-$dbh = new PDO("pgsql:dbname=$dbname;host=$host;username=$username;password=$passwd");
+$dbh = new PDO("pgsql:dbname=d35djr9rl60o00;host=ec2-23-21-94-99.compute-1.amazonaws.com","ptplxvkmvcsjez","27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872");
 
 }catch(Exception $e){
 echo $e->getMessage();
