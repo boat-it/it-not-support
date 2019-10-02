b4ca55472fb896f1d6cb878d330b8a8811eb4a75
boat-it <55787836+boat-it@users.noreply.github.com>
2019-10-02 11:18:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -149,22 +149,6 @@ array(
 );
 
 
-
-
-function send_reply_message($url, $post_header, $post_body)
-{
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-    return $result;
-}
-
 if ( sizeof($request_array['events']) > 0 ) {
     foreach ($request_array['events'] as $event) {
     
@@ -192,8 +176,17 @@ print "OK";
 
 
 
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
 
-
-
-
+    return $result;
+}
 ?>
