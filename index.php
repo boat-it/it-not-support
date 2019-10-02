50d191259fc44881853c0fd513dff0654a700104
boat-it <55787836+boat-it@users.noreply.github.com>
2019-10-02 09:58:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,139 +14,139 @@ $request_array = json_decode($request, true);
 
 
 
-$jsonFlex = [ 
-"type" => "flex", 
-"altText" => "Flex Message", 
-"contents" => [ 
-"type" => "bubble", 
-"direction" => "rtl", 
-"header" => [ 
-"type" => "box", 
-"layout" => "vertical", 
-"contents" => [ 
-[ 
-"type" => "text", 
-"text" => "แจ้งงาน IT-support", 
-"margin" => "xl", 
-"size" => "md", 
-"align" => "center", 
-"gravity" => "center", 
-"weight" => "bold", 
-"color" => "#005FFF" 
-] 
-] 
-], 
-"body" => [ 
-"type" => "box", 
-"layout" => "vertical", 
-"contents" => [ 
-[ 
-"type" => "spacer", 
-"size" => "xs" 
-], 
-[ 
-"type" => "box", 
-"layout" => "vertical", 
-"contents" => [ 
-[ 
-"type" => "filler" 
-], 
-[ 
-"type" => "box", 
-"layout" => "horizontal", 
-"contents" => [ 
-[ 
-"type" => "text", 
-"text" => "ขอ Reset Password เนื่องจาก รหัสผ่าน ของ nannapat หมดอายุ", 
-"margin" => "none", 
-"align" => "end", 
-"wrap" => true 
-], 
-[ 
-"type" => "text", 
-"text" => " =>Detail", 
-"flex" => 0, 
-"align" => "end", 
-"weight" => "bold" 
-] 
-] 
-], 
-[ 
-"type" => "box", 
-"layout" => "horizontal", 
-"contents" => [ 
-[ 
-"type" => "text", 
-"text" => "ห้องบัญชี เครื่องชั่ง", 
-"align" => "end", 
-"gravity" => "top", 
-"color" => "#545454", 
-"wrap" => true 
-], 
-[ 
-"type" => "text", 
-"text" => " =>Location", 
-"flex" => 0, 
-"align" => "end", 
-"gravity" => "top", 
-"weight" => "bold" 
-] 
-] 
-], 
-[ 
-"type" => "box", 
-"layout" => "horizontal", 
-"spacing" => "none", 
-"contents" => [ 
-[ 
-"type" => "text", 
-"text" => "nannapat", 
-"flex" => 1, 
-"align" => "end" 
-], 
-[ 
-"type" => "text", 
-"text" => " =>Username", 
-"flex" => 0, 
-"size" => "sm", 
-"align" => "end", 
-"weight" => "bold", 
-"wrap" => false 
-] 
-] 
-] 
-] 
-] 
-] 
-], 
-"footer" => [ 
-"type" => "box", 
-"layout" => "horizontal", 
-"contents" => [ 
-[ 
-"type" => "text", 
-"text" => "192.168.5.6", 
-"align" => "center", 
-"gravity" => "center" 
-], 
-[ 
-"type" => "button", 
-"action" => [ 
-"type" => "uri", 
-"label" => "ติดตามงาน", 
-"uri" => "http =>//192.168.5.10/dailyreportpm1" 
-], 
-"flex" => 0, 
-"color" => "#089AFF", 
-"margin" => "none", 
-"height" => "md", 
-"style" => "link", 
-"gravity" => "center" 
-] 
-] 
-] 
-] 
-];
+$jsonFlex = array( 
+"type"=> "flex", 
+"altText"=> "Flex Message", 
+"contents"=> array( 
+"type"=> "bubble", 
+"direction"=> "rtl", 
+"header"=> array( 
+"type"=> "box", 
+"layout"=> "vertical", 
+"contents"=> array( 
+                    array( 
+                    "type"=> "text", 
+                    "text"=> "แจ้งงาน IT-support", 
+                    "margin"=> "xl", 
+                    "size"=> "md", 
+                    "align"=> "center", 
+                    "gravity"=> "center", 
+                    "weight"=> "bold", 
+                    "color"=> "#005FFF" 
+                    ) 
+) 
+), 
+"body"=> array( 
+"type"=> "box", 
+"layout"=> "vertical", 
+"contents"=> array( 
+array( 
+"type"=> "spacer", 
+"size"=> "xs" 
+), 
+array( 
+"type"=> "box", 
+"layout"=> "vertical", 
+"contents"=> array( 
+array( 
+"type"=> "filler" 
+), 
+array( 
+"type"=> "box", 
+"layout"=> "horizontal", 
+"contents"=> array( 
+array( 
+"type"=> "text", 
+"text"=> "ขอ Reset Password เนื่องจาก รหัสผ่าน ของ nannapat หมดอายุ", 
+"margin"=> "none", 
+"align"=> "end", 
+"wrap"=> true 
+), 
+array( 
+"type"=> "text", 
+"text"=> "=>Detail", 
+"flex"=> 0, 
+"align"=> "end", 
+"weight"=> "bold" 
+) 
+) 
+), 
+array( 
+"type"=> "box", 
+"layout"=> "horizontal", 
+"contents"=> array( 
+array( 
+"type"=> "text", 
+"text"=> "ห้องบัญชี เครื่องชั่ง", 
+"align"=> "end", 
+"gravity"=> "top", 
+"color"=> "#545454", 
+"wrap"=> true 
+), 
+array( 
+"type"=> "text", 
+"text"=> "=>Location", 
+"flex"=> 0, 
+"align"=> "end", 
+"gravity"=> "top", 
+"weight"=> "bold" 
+) 
+) 
+), 
+array( 
+"type"=> "box", 
+"layout"=> "horizontal", 
+"spacing"=> "none", 
+"contents"=> array( 
+array( 
+"type"=> "text", 
+"text"=> "nannapat", 
+"flex"=> 1, 
+"align"=> "end" 
+), 
+array( 
+"type"=> "text", 
+"text"=> "=>Username", 
+"flex"=> 0, 
+"size"=> "sm", 
+"align"=> "end", 
+"weight"=> "bold", 
+"wrap"=> false 
+) 
+) 
+) 
+) 
+) 
+) 
+), 
+"footer"=> array( 
+"type"=> "box", 
+"layout"=> "horizontal", 
+"contents"=> array( 
+array( 
+"type"=> "text", 
+"text"=> "192.168.5.6", 
+"align"=> "center", 
+"gravity"=> "center" 
+), 
+array( 
+"type"=> "button", 
+"action"=> array( 
+"type"=> "uri", 
+"label"=> "ติดตามงาน", 
+"uri"=> "http=>//192.168.5.10/dailyreportpm1" 
+), 
+"flex"=> 0, 
+"color"=> "#089AFF", 
+"margin"=> "none", 
+"height"=> "md", 
+"style"=> "link", 
+"gravity"=> "center" 
+) 
+) 
+) 
+) 
+);
           
 
 
