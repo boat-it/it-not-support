74e19d4370e42bd87cdc8da69774b495e8e938ea
boat-it <55787836+boat-it@users.noreply.github.com>
2019-10-02 10:52:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -178,7 +178,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 //     ];
                 $data = [
             'replyToken' => $reply_token,
-            'messages' => [$jsonFlex]
+            'messages' => $jsonFlex
         ];
         print_r($data);
     $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
