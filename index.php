b4810b80eba3b1381ecbea62db96bf58e6548086
boat-it <55787836+boat-it@users.noreply.github.com>
2019-10-02 11:13:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -185,8 +185,9 @@ if ( sizeof($request_array['events']) > 0 ) {
     $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
     $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);      echo "Result: ".$send_result."\r\n";   }
 }
-print_r($data);
-echo "TEST";
+print_r($post_body);
+print "OK";
+
 
 
 
