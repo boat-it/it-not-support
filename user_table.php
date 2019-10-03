4156024c3c898cefcbc82fd34a684258e54aa1f8
boat-it <55787836+boat-it@users.noreply.github.com>
2019-10-03 14:51:22 +0700

    Update user_table.php

--- user_table.php
+++ user_table.php
@@ -3,10 +3,15 @@ include('connectstring.php');
 $stmt=$dbh->query("select * from user_table");
 $stmt->execute();
 print "<table border=1 width=50%>";
+print "<tr>";
+print "<th>ID</th>";
+print "<th>USER NAME</th>";
+print "<th>ID@LINE</th>";
 while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
           print "<tr>";
-          print "<td>$row[id]<td>";
-         print "<td>$row[username]<td>";
+          print "<td>$row[id]</td>";
+         print "<td>$row[username]</td>";
+         print "<td>$row[idline]</td>";
           print "</tr>";
 }
 ?>
