b187c91be7ad527a0edd3b2313eed971a49fd113
boat-it <botzacabal16@gmail.com>
2019-10-18 14:22:41 +0700

    this work

--- user_table.php
+++ user_table.php
@@ -1,15 +1,12 @@
 <?php
-require_once 'connectstring.php';
-try{
-$stmt=$dbh->query("SELECT * from user_table")->execute();
+include('connectstring.php');
+$stmt=$dbh->query("select * from user_table");
+$stmt->execute();
 print "<table border=1 width=50%>";
 print "<tr>";
 print "<th>ID</th>";
 print "<th>USER NAME</th>";
 print "<th>ID@LINE</th>";
-}catch(Exception $e){
-    echo $e->getMessage();
-}
 while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
           print "<tr>";
           print "<td>$row[id]</td>";
