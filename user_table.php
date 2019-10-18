99d96af2c6e51b35b1d9eaa46208af887a3577ae
boat-it <botzacabal16@gmail.com>
2019-10-18 14:47:16 +0700

    add hearder table

--- user_table.php
+++ user_table.php
@@ -5,7 +5,9 @@ $stmt->execute();
 print "<table border=1 width=50%>";
 print "<th>ID</th>";
 print "<th>USER NAME</th>";
-print "<th>@LINE ID</th>";
+print "<th>สถานะทำงาน</th>";
+print "<th>กรรมเช้า/บ่าย</th>";
+print "<th>สถานะ(รอบปัจจุบัน)</th>";
 while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
     print_r($row);
     print "<tr>";
