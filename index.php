e986e5e7e448489f1567671a867481ba8ddbdc26
boat-it <55787836+boat-it@users.noreply.github.com>
2019-10-21 21:09:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ include 'callAPI.php';
     if($message == "Testing"){
         $arrayPostData['to'] = $id;
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "Testing OK ";
+        $arrayPostData['messages'][0]['text'] = "Testing OK $id";
         $arrayPostData['messages'][1]['type'] = "sticker";
         $arrayPostData['messages'][1]['packageId'] = "2";
         $arrayPostData['messages'][1]['stickerId'] = "34";
