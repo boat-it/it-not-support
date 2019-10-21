5eb6606f180a4d37249890a16f830a7d7098bac9
boat-it <botzacabal16@gmail.com>
2019-10-21 15:03:55 +0700

    Update check.php
    ทดสอบ cronjob on heroku server

--- example-code/check.php
+++ example-code/check.php
@@ -1,8 +1,16 @@
 <?php
 include 'connectstring.php';
 $current_date=date("Y-m-d H:i:s");
-$query="INSERT into chech_date_time($current_date) value (:current_date)";
+$query="INSERT into check_date_time('current_date') value (:current_date)";
 $stmt=$dbh->prepare($query);
 $stmt->bindParam(':current_date',$current_date);
 $stmt->execute();
+print "<hr>";
+$query2="SELECT * from check_date_time order by current_date ASC";
+$stmt=$dbh->query($query2);
+$stmt->execute();
+while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+    print $row['current_date'];
+    print "<br>";
+}
 ?>
