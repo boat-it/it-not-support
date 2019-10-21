ce1b19aa34b68815bd67bcb842dbe436253bbb6b
boat-it <botzacabal16@gmail.com>
2019-10-21 17:23:39 +0700

    Update check.php

--- example-code/check.php
+++ example-code/check.php
@@ -4,7 +4,7 @@ date_default_timezone_set("Asia/Bangkok");
 $current_date=date("Y-m-d");
 $query="INSERT into check_date_time('current_date') VALUES ($current_date)";
 $stmt=$dbh->prepare($query);
-$stmt->bindColumn(':current_date',$current_date);
+$stmt->bindValue(':current_date',$current_date);
 $stmt->execute();
 print "<hr>";
 $query2="SELECT * from check_date_time order by current_date ASC";
