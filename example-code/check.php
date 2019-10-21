b3b9bd9e2c2b056606e91829d60457f0de8a92e5
boat-it <botzacabal16@gmail.com>
2019-10-21 17:31:51 +0700

    Update check.php
    extend $stmt 1,2

--- example-code/check.php
+++ example-code/check.php
@@ -3,14 +3,14 @@ include '../connectstring.php';
 date_default_timezone_set("Asia/Bangkok");
 print "current_date".$current_date=date("Y-m-d H:i:s");
 $query="INSERT into check_date_time('current_date') VALUES ($current_date)";
-$stmt=$dbh->prepare($query);
-$stmt->bindParam(':current_date',$current_date,PDO::PARAM_STR);
-$stmt->execute();
+$stmt1=$dbh->prepare($query);
+$stmt1->bindParam(':current_date',$current_date,PDO::PARAM_STR);
+$stmt1->execute();
 print "<hr>";
 $query2="SELECT * from check_date_time order by current_date ASC";
-$stmt=$dbh->query($query2);
-$stmt->execute();
-while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+$stmt2=$dbh->query($query2);
+$stmt2->execute();
+while($row=$stmt2->fetch(PDO::FETCH_ASSOC)){
     print $row['current_date'];
     print "<br>";
 }
