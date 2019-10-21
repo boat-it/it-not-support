4670767a77c339d193e24e05a53d81e512eeb5da
boat-it <botzacabal16@gmail.com>
2019-10-22 05:28:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -75,123 +75,86 @@ include 'callAPI.php';
     }
     // flex message 
     $jsonFlex = [
-        "type" => "flex",
-        "altText" => "Hello Flex Message",
-        "contents" => [
-          "type" => "bubble",
-          "direction" => "ltr",
-          "header" => [
-            "type" => "box",
-            "layout" => "vertical",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "Purchase",
-                "size" => "lg",
-                "align" => "start",
-                "weight" => "bold",
-                "color" => "#009813"
-              ],
-              [
-                "type" => "text",
-                "text" => "฿ 100.00",
-                "size" => "3xl",
-                "weight" => "bold",
-                "color" => "#000000"
-              ],
-              [
-                "type" => "text",
-                "text" => "Rabbit Line Pay",
-                "size" => "lg",
-                "weight" => "bold",
-                "color" => "#000000"
-              ],
-              [
-                "type" => "text",
-                "text" => "2019.02.14 21:47 (GMT+0700)",
-                "size" => "xs",
-                "color" => "#B2B2B2"
-              ],
-              [
-                "type" => "text",
-                "text" => "Payment complete.",
-                "margin" => "lg",
-                "size" => "lg",
-                "color" => "#000000"
-              ]
-            ]
-          ],
-          "body" => [
-            "type" => "box",
-            "layout" => "vertical",
-            "contents" => [
-              [
-                "type" => "separator",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "box",
-                "layout" => "baseline",
-                "margin" => "lg",
-                "contents" => [
+            "type"=> "flex",
+            "altText"=> "กรรมการรับเศษกระดาษ",
+            "contents"=> [
+              "type"=> "bubble",
+              "direction"=> "ltr",
+              "header"=> [
+                "type"=> "box",
+                "layout"=> "vertical",
+                "contents"=> [
                   [
-                    "type" => "text",
-                    "text" => "Merchant",
-                    "align" => "start",
-                    "color" => "#C3C3C3"
-                  ],
-                  [
-                    "type" => "text",
-                    "text" => "BTS 01",
-                    "align" => "end",
-                    "color" => "#000000"
+                    "type"=> "text",
+                    "text"=> "กรรมการรับเศษประจำวัน",
+                    "size"=> "lg",
+                    "align"=> "center",
+                    "gravity"=> "top",
+                    "weight"=> "bold",
+                    "color"=> "#0032FF"
                   ]
                 ]
               ],
-              [
-                "type" => "box",
-                "layout" => "baseline",
-                "margin" => "lg",
-                "contents" => [
+              "body"=> [
+                "type"=> "box",
+                "layout"=> "vertical",
+                "contents"=> [
                   [
-                    "type" => "text",
-                    "text" => "New balance",
-                    "color" => "#C3C3C3"
+                    "type"=> "box",
+                    "layout"=> "horizontal",
+                    "contents"=> [
+                      [
+                        "type"=> "text",
+                        "text"=> "1.-",
+                        "flex"=> "0",
+                        "align"=> "center",
+                        "weight"=> "bold"
+                      ],
+                      [
+                        "type"=> "text",
+                        "text"=> "(username 1) (id-line)",
+                        "margin"=> "sm",
+                        "align"=> "start"
+                      ]
+                    ]
                   ],
                   [
-                    "type" => "text",
-                    "text" => "฿ 45.57",
-                    "align" => "end"
+                    "type"=> "box",
+                    "layout"=> "horizontal",
+                    "contents"=> [
+                      [
+                        "type"=> "text",
+                        "text"=> "2.-",
+                        "flex"=> "0",
+                        "weight"=> "bold"
+                      ],
+                      [
+                        "type"=> "text",
+                        "text"=> "(username 2) (id-line)",
+                        "margin"=> "sm",
+                        "align"=> "start"
+                      ]
+                    ]
                   ]
                 ]
               ],
-              [
-                "type" => "separator",
-                "margin" => "lg",
-                "color" => "#C3C3C3"
-              ]
-            ]
-          ],
-          "footer" => [
-            "type" => "box",
-            "layout" => "horizontal",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "View Details",
-                "size" => "lg",
-                "align" => "start",
-                "color" => "#0084B6",
-                "action" => [
-                  "type" => "uri",
-                  "label" => "View Details",
-                  "uri" => "https://google.co.th/"
+              "footer"=> [
+                "type"=> "box",
+                "layout"=> "horizontal",
+                "contents"=> [
+                  [
+                    "type"=> "button",
+                    "action"=> [
+                      "type"=> "uri",
+                      "label"=> "Button",
+                      "uri"=> "https=>//linecorp.com"
+                    ]
+                  ]
                 ]
               ]
             ]
-          ]
-        ]
-      ];
+       ];
+       
     // 
     if($message == "Testing"){
 
