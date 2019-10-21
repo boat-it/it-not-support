bb8690c71af704b0690933b856f4cc235d4a662b
boat-it <botzacabal16@gmail.com>
2019-10-21 09:34:48 +0700

    Update check.php

--- example-code/check.php
+++ example-code/check.php
@@ -1,17 +1,17 @@
 <?php
 include '../connectstring.php';
-// $current_date=date("Y-m-d H:i:s");
-// $query="INSERT into check_date_time(current_date) VALUES ($current_date)";
-// $stmt=$dbh->query($query);
-// $stmt->execute();
-// print "<hr>";
-// $query2="SELECT * from check_date_time order by current_date ASC";
-// $stmt=$dbh->query($query2);
-// $stmt->execute();
-// while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
-//     print $row['current_date'];
-//     print "<br>";
-// }
+$current_date=date("Y-m-d H:i:s");
+$query="INSERT into check_date_time('current_date') VALUES ($current_date)";
+$stmt=$dbh->query($query);
+$stmt->execute();
+print "<hr>";
+$query2="SELECT * from check_date_time order by current_date ASC";
+$stmt=$dbh->query($query2);
+$stmt->execute();
+while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+    print $row['current_date'];
+    print "<br>";
+}
 print "file ok";
 
 ?>
