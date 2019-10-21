4d63df724afd6b3b37534465559719c7e425da9b
boat-it <botzacabal16@gmail.com>
2019-10-21 17:39:51 +0700

    Update check.php

--- example-code/check.php
+++ example-code/check.php
@@ -2,9 +2,9 @@
 include '../connectstring.php';
 date_default_timezone_set("Asia/Bangkok");
 print "current_date".$current_date=date("Y-m-d H:i:s");
-$query="INSERT into check_date_time('current_date') VALUES (:current_date)";
+$query="INSERT into check_date_time(current_date) VALUES (:current_date)";
 $stmt1=$dbh->prepare($query);
-$stmt1->bindParam(':current_date',$current_date,PDO::PARAM_STR);
+$stmt1->bindParam(':current_date',$current_date);
 $stmt1->execute();
 print "<hr>";
 $query2="SELECT * from check_date_time order by current_date ASC";
