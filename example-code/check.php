ed88889347b81896fb8b25b7f767075a3654ec40
boat-it <botzacabal16@gmail.com>
2019-10-21 17:30:35 +0700

    Update check.php
    add PDO::PARAM_STR ต่อ bindParam
    :+1:

--- example-code/check.php
+++ example-code/check.php
@@ -4,7 +4,7 @@ date_default_timezone_set("Asia/Bangkok");
 print "current_date".$current_date=date("Y-m-d H:i:s");
 $query="INSERT into check_date_time('current_date') VALUES ($current_date)";
 $stmt=$dbh->prepare($query);
-$stmt->bindParam(':current_date',$current_date);
+$stmt->bindParam(':current_date',$current_date,PDO::PARAM_STR);
 $stmt->execute();
 print "<hr>";
 $query2="SELECT * from check_date_time order by current_date ASC";
