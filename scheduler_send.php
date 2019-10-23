450dd7875eb6859c56d38e202a5234ad8c9a6633
boat-it <botzacabal16@gmail.com>
2019-10-23 10:36:35 +0700

    Update scheduler_send.php
    ไฟล์ ตั้งเวลา ทำงาน heroku scheduler

--- scheduler_send.php
+++ scheduler_send.php
@@ -1,3 +1,20 @@
 <?php
-// sql 
-?>
+include 'connectstring.php';
+$current=date("Y-m-d H:i:s");
+if(isset($current)!=''){
+try{
+$query="INSERT INTO check_date_time('current_date') values (:current)";
+$stmt=$dbh->prepare($query);
+$stmt->bindParam(':current',$current);
+$stmt->execute();
+}catch(Exception $e){
+    echo $e->getMessage();
+}
+}
+// 
+$query="SELECT * from check_date_time";
+$stmt=$dbh->query($query);
+$stmt->execute();
+while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+    print_r($row);
+}
