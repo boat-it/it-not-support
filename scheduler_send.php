ba8aebbcd0320342bd3848e33b633fd4506e19c1
boat-it <botzacabal16@gmail.com>
2019-10-23 17:04:00 +0700

    Update scheduler_send.php
    cancel update datebase check if else

--- scheduler_send.php
+++ scheduler_send.php
@@ -12,11 +12,7 @@ function convertdatetimezone($date ,$format = 'Y-m-d H:i:s'){
 print "GMT:DATE=".convertdatetimezone($current);
 $currentgmt=convertdatetimezone($current);
 $query="INSERT INTO check_date_time (current_date) VALUES ($currentgmt)";
-if(!$temp=pg_query($dbcon_old,$query)){
-    printf("Error:%s\n",$temp);
-}else{
-    echo "inserted";
-}
+$temp=pg_query($dbcon_old,$query);
 // 
 print "<hr>";
 $query="SELECT * from check_date_time";
