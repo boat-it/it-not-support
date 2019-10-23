62a864630b87c7ce68bf4574ef9324e713156463
boat-it <botzacabal16@gmail.com>
2019-10-23 12:49:43 +0700

    ีupdate scheduler.php

--- scheduler_send.php
+++ scheduler_send.php
@@ -4,12 +4,10 @@ $current=date("Y-m-d H:i:s");
 print "current_date:$current";
 print "<hr>";
 try{
-if (isset($current)) {
     $query="INSERT INTO check_date_time (current_dat) values (:current)";
     $stmt=$dbh->prepare($query);
-    $data=array('current'=>$current);
-    $stmt->execute($data);
-}
+    $stmt->bindParam(':current',$current);
+    $stmt->execute();
 }catch(Exception $e){
     echo $e->getMessage();
 }
