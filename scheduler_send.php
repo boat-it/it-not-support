f41bdda9067fb73748dcea4f7b00ac976dcb5915
boat-it <botzacabal16@gmail.com>
2019-10-23 13:02:19 +0700

    Update scheduler_send.php
    convert time zone

--- scheduler_send.php
+++ scheduler_send.php
@@ -1,12 +1,20 @@
 <?php
 include 'connectstring.php';
 $current=date("Y-m-d H:i:s");
-print "current_date:$current";
-print "<hr>";
+function convertdatetimezone($date ,$format = 'Y-m-d H:i:s'){
+    $tz1= 'UTC';
+    $tz2= 'Asia/Bangkok';
+    $d= new DateTime($date, new DateTimeZone($tz1));
+    $d->setTimezone(new DateTimeZone($tz2));
+
+    return $d->format($format);
+}
+print "GMT:DATE=".convertdatetimezone($current);
+$currentgmt=convertdatetimezone($current);
 try{
     $query="INSERT INTO check_date_time (current_dat) values (:current)";
     $stmt=$dbh->prepare($query);
-    $stmt->bindParam(':current',$current);
+    $stmt->bindParam(':current',$currentgmt);
     $stmt->execute();
 }catch(Exception $e){
     echo $e->getMessage();
@@ -19,3 +27,4 @@ while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
     print_r($row);
     print "<br>";
 }
+?>
