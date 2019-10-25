a75a7b2593e79dd2bf3b3422ad1d058a96dd2ffb
boat-it <botzacabal16@gmail.com>
2019-10-25 15:53:28 +0700

    Update scheduler_send.php

--- scheduler_send.php
+++ scheduler_send.php
@@ -10,6 +10,13 @@ function convertdatetimezone($date ,$format = 'Y-m-d H:i:s'){
     return $d->format($format);
 }
 $currentgmt=convertdatetimezone($current);
+$querys="SELECT * from testinsert";
+$stmt2=$dbh->prepare($querys);
+$stmt2->execute();
+while ($row=$stmt2->fetch(PDO::FETCH_ASSOC)) {
+    print_r($row);
+}
+
 $query="SELECT * from testinsert order by id_master DESC limit 1";
 $stmt=$dbh->prepare($query);
 $stmt->execute();
@@ -18,6 +25,8 @@ print"line1". $count=$rownum['id_master'];
 $num=1;
 if($num>=$count){
 $sumnum=$num+$count;
+}else{
+    $sumnum=1;
 }
 try {
         $query="INSERT into testinsert values(:id_master,:test_data2)";
@@ -26,12 +35,7 @@ try {
         $stmt->bindParam(':test_data2', $currentgmt);
         $stmt->execute();
         // 
-        $querys="SELECT * from testinsert";
-        $stmt2=$dbh->prepare($querys);
-        $stmt2->execute();
-        while ($row=$stmt2->fetch(PDO::FETCH_ASSOC)) {
-            print_r($row);
-        }
+
     }catch(Exception $e){
         echo $e->getmessage();
     }
