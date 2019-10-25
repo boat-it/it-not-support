7036805d5ed9b5537e84127f83987640f5ee2afe
boat-it <botzacabal16@gmail.com>
2019-10-25 14:19:40 +0700

    Update connectstring.php

--- connectstring.php
+++ connectstring.php
@@ -1,14 +1,33 @@
 <?php
-$db = parse_url(getenv("postgres://ptplxvkmvcsjez:27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872@ec2-23-21-94-99.compute-1.amazonaws.com:5432/d35djr9rl60o00"));
-try{
-    $dbname="d35djr9rl60o00";
-    $host="ec2-23-21-94-99.compute-1.amazonaws.com";
-    $username="ptplxvkmvcsjez";
-    $passwd="27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872";
-    $dbh = new PDO("pgsql:dbname=d35djr9rl60o00;host=ec2-23-21-94-99.compute-1.amazonaws.com","ptplxvkmvcsjez","27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872");
-}catch(Exception $e){
-    echo $e->getMessage();
-}
+// $db = parse_url(getenv("postgres://ptplxvkmvcsjez:27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872@ec2-23-21-94-99.compute-1.amazonaws.com:5432/d35djr9rl60o00"));
+// try{
+//     $dbname="d35djr9rl60o00";
+//     $host="ec2-23-21-94-99.compute-1.amazonaws.com";
+//     $username="ptplxvkmvcsjez";
+//     $passwd="27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872";
+//     $dbh = new PDO("pgsql:dbname=d35djr9rl60o00;host=ec2-23-21-94-99.compute-1.amazonaws.com","ptplxvkmvcsjez","27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872");
+// }catch(Exception $e){
+//     echo $e->getMessage();
+// }
 
-$conn_old="host=ec2-23-21-94-99.compute-1.amazonaws.com port=5432 dbname=d35djr9rl60o00 user=ptplxvkmvcsjez password=27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872";
-$dbcon_old=pg_connect($conn_old);
+// $conn_old="host=ec2-23-21-94-99.compute-1.amazonaws.com port=5432 dbname=d35djr9rl60o00 user=ptplxvkmvcsjez password=27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872";
+// $dbcon_old=pg_connect($conn_old);
+
+$hostname='ec2-23-21-94-99.compute-1.amazonaws.com';
+$username='ptplxvkmvcsjez';
+$password='27151faae2a16ce43680f647bf4387ca2bc91c8c6159944f8b8c9bcc649ce872';
+
+try{
+    $dbh = new PDO("pgsql:host=$hostname;dbname=d35djr9rl60o00",$username,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo 'Connect to Database<br/>';
+    $sql="SELECT * from user_table";
+    $stmt=$dbh->prepare($sql);
+    $stmt->execute();
+    foreach($dbh->query($sql) as $row){
+        print_r($row);
+    }
+}catch(Exception $e){
+    echo $e->getMessage(),$e->getLine();
+}
+?>
