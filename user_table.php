808d5144bbd01627361050487935863b87d81b0c
boat-it <botzacabal16@gmail.com>
2019-10-26 15:15:44 +0700

    Update user_table.php

--- user_table.php
+++ user_table.php
@@ -2,32 +2,14 @@
 <?php
 include('connectstring.php');
 if (isset($_GET['id'])){
-
-    $stmt=$dbh->query("SELECT * from user_table a left join department b on a.username = b.username where a.id='$_GET[id]' order by a.id ASC limit 1");
-    $stmt->execute();
-    // print "<table border=1 width=50%>";
-    // print "<th>ID</th>";
-    // print "<th>USER NAME</th>";
-    // print "<th>สถานะทำงาน</th>";
-    // print "<th>กรรมเช้า/บ่าย</th>";
-    // print "<th>สถานะ(รอบปัจจุบัน)</th>";
-    // print "<th>แผนก</th>";
-    // print "<th>เพศ</th>";
-    while ($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
-        print $row['username'];
-        // print_r($row);
-        // print "<tr>";
-        // print "<td>$row[id]</td>";
-        // print "<td>$row[username]</td>";
-        // print "<td>$row[id_line]</td>";
-        // print "<td>".($row['status1']==1?"เช้า":"บ่าย")."</td>";
-        // print "<td>".($row['status2']==2? "เลื่อน":"ลงเเล้ว")."</td>";
-        // print "<td>$row[board]</td>";
-        // print "<td>$row[department]</td>";
-        // print "<td>$row[sex]</td>";
-        // print "<td><button valeu='edit' id='btn_edit'>edit</button></td>";
-        // print "</tr>";
-        // print json_encode($row);
+    try {
+        $stmt=$dbh->query("SELECT * from user_table a left join department b on a.username = b.username where a.id='$_GET[id]' order by a.id ASC limit 1");
+        $stmt->execute();
+        while ($row=$stmt->fetch(PDO::FETCH_ASSOC)) {
+            print $row['username'];
+        }
+    }catch(Exception $e){
+        echo $e->getMessage();
     }
 }
 ?>
