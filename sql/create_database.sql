563982fa77ca23eb9d75d9bceec5b74d4c6dde30
boat-it <botzacabal16@gmail.com>
2019-10-26 11:13:00 +0700

    add type .sql

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE check_date_time
 (
-    'current_date' timestamp without time zone NOT NULL,
-    CONSTRAINT check_date_time_pkey PRIMARY KEY ('current_date')
+    current_date timestamp without time zone NOT NULL,
+    CONSTRAINT check_date_time_pkey PRIMARY KEY (current_date)
 )
 WITH (
     OIDS = FALSE
