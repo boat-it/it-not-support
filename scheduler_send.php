630f86d3701416883fb53ef335320419a86a8f6d
it-support <botzacabal16@gmail.com>
2020-01-14 11:26:21 +0700

    Update scheduler_send.php

--- scheduler_send.php
+++ scheduler_send.php
@@ -26,6 +26,8 @@ include 'connectstring.php';
    print_r($arrayJson);
    if(isset($arrayJson['events'][0]['source']['userId'])){
       $id = $arrayJson['events'][0]['source']['userId'];
+      $idgroup = $arrayJson['events'][0]['source']['groupId'];
+      $idroom = $arrayJson['events'][0]['source']['room'];
    }
    else if(isset($arrayJson['events'][0]['source']['groupId'])){
       $idgroup = $arrayJson['events'][0]['source']['groupId'];
@@ -41,7 +43,7 @@ include 'connectstring.php';
       $arrayPostData['messages'][1]['packageId'] = "2";
       $arrayPostData['messages'][1]['stickerId'] = "34";
       $arrayPostData['messages'][2]['type'] = 'text';
-      $arrayPostData['messages'][2]['text'] = "ID{$group}";
+      $arrayPostData['messages'][2]['text'] = "group[$idgroup] : room[$idroom]";
       pushMsg($arrayHeader,$arrayPostData);
    function pushMsg($arrayHeader,$arrayPostData){
       $strUrl = "https://api.line.me/v2/bot/message/push";$ch = curl_init();
