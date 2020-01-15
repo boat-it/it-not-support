e8bb69a9ffd1a312b299ab614419a10a89817187
it-support <botzacabal16@gmail.com>
2020-01-15 07:14:49 +0700

    Update index.php

--- app/index.php
+++ app/index.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link src='stylesheet' href='../library/css/bootstrap.min.css'>
     <link src='stylesheet' href='../library/css/jquery.3.4.1.min.css'>
-    <script src="../library/js/jquery.3.4.1.min.js"></script>
+    <!-- <script src="../library/js/jquery.3.4.1.min.js"></script> -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="../library/js/bootstrap.min.js"></script>
     <title>management</title>
 </head>
