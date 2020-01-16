20799361c63f8283c856226d6611c39938ede8d4
it-support <botzacabal16@gmail.com>
2020-01-16 16:03:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,249 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Title</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link src='stylesheet' href='library/css/bootstrap.min.css'>
+    <link src='stylesheet' href='library/css/jquery.3.4.1.min.css'>
+    <script src="library/js/bootstrap.min.js"></script>
+    <script src="library/js/jquery.3.4.1.min.js"></script>
+    <title>user table committee</title>
+</head>
+<body>
 <?php
-include 'connectstring.php'; // postgres connect
-$accessToken = "kT9H2mrXWPMGeaTwwUpqu3RXRTTghlSAHXaPk+jZWC7kW8lI9pkbi8po6wemhLv3wzp7FUnh52sTOYbu+b1pPWMTIkGuqEuKAG2h3oqHFtkc23sSukoDHo6+o2e64a01J00m0JVo4h4wM2jDD+r2bQdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
-$channelSecret = '0e14c3f144f47fbc2d247184253c0bf6';
-$content = file_get_contents('php://input');
-$arrayJson = json_decode($content, true);
-$message = $arrayJson['events'][0]['message']['text'];    
-    $json_array = json_decode($_POST['json']);
+include 'callAPI.php';
+include 'connectstring.php';
+date_default_timezone_set("Asia/Bangkok");
+$date = date("Y-m-d");
+$time = date("H:i:s");
+
+    $accessToken = "kT9H2mrXWPMGeaTwwUpqu3RXRTTghlSAHXaPk+jZWC7kW8lI9pkbi8po6wemhLv3wzp7FUnh52sTOYbu+b1pPWMTIkGuqEuKAG2h3oqHFtkc23sSukoDHo6+o2e64a01J00m0JVo4h4wM2jDD+r2bQdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    $channelSecret = '0e14c3f144f47fbc2d247184253c0bf6';
+
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+
+    // $arrayHeader = ['Content-Type: application/json','Authorization: Bearer'.$accessToken];
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+
+    //รับข้อความจากผู้ใช้
+    $message = $arrayJson['events'][0]['message']['text'];#ตัวอย่าง Message Type "Text"
+    if (strpos($message,"สวัสดี") == "สวัสดี") {
+        
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        replyMsg($arrayHeader, $arrayPostData);
+    }
+
+    
+    if (strpos($message,"จ้า") == true || strpos($message,"Ok") == true && strpos($message,"เลื่อนค่ะ") == false && strpos($message,"เลื่อนคับ")==false) {
+        $rand_id=rand(01, 04);
+        $rand_sex=rand(01, 02);
+        $rand_department=rand(01, 04);
+        $arrayPostData['to']='Ub506b7d4a2ed187ebf8e0fbb22134d40';
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $get_data = callAPI('GET', 'https://it-not-support.herokuapp.com/user_table.php?id='.$rand_id, false);
+        $response = json_decode($get_data, true);
+        // $errors = $response['response']['errors'];
+        // $data = $response['response']['data'][0];
+        $arrayPostData['messages'][0]['text'] = "callAPI:$get_data";
+        replyMsg($arrayHeader, $arrayPostData);
+    }
+    //
+    #ตัวอย่าง Message Type "Sticker"
+    elseif ($message == "ฝันดี") {
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "sticker";
+        $arrayPostData['messages'][0]['packageId'] = "2";
+        $arrayPostData['messages'][0]['stickerId'] = "46";
+        replyMsg($arrayHeader, $arrayPostData);
+    }
+    // test show group
+    if (isset($arrayJson['events'][0]['source']['userId'])) {
+        $id = $arrayJson['events'][0]['source']['userId'];
+    } elseif (isset($arrayJson['events'][0]['source']['groupId'])) {
+        $id = $arrayJson['events'][0]['source']['groupId'];
+    } elseif (isset($arrayJson['events'][0]['source']['room'])) {
+        $id = $arrayJson['events'][0]['source']['room'];
+    }
+    //  USER ID LINE
+    // $userId = $arrayJson['originalDetectIntentRequest']['payload']['data']['source']['userId'];
+    // 
+    // flex message
+    $rand_id=rand(01, 04); //random user from database
+    $get_data = callAPI('GET', 'https://it-not-support.herokuapp.com/user_table.php?id='.$rand_id, false);
+    $response = json_decode($get_data, true);
+
+  //   $json_array='[{
+  //     "type": "flex",
+  //     "altText": "Flex Message",
+  //     "contents": {
+  //       "type": "bubble",
+  //       "direction": "ltr",
+  //       "header": {
+  //         "type": "box",
+  //         "layout": "vertical",
+  //         "contents": [
+  //           {
+  //             "type": "text",
+  //             "text": "กรรมการรับเศษ(รอบเช้า)",
+  //             "size": "lg",
+  //             "align": "center",
+  //             "gravity": "top",
+  //             "weight": "bold",
+  //             "color": "#FFFFFF",
+  //             "wrap": true
+  //           }
+  //         ]
+  //       },
+  //       "body": {
+  //         "type": "box",
+  //         "layout": "vertical",
+  //         "contents": [
+  //           {
+  //             "type": "box",
+  //             "layout": "horizontal",
+  //             "contents": [
+  //               {
+  //                 "type": "text",
+  //                 "text": "1.-",
+  //                 "flex": 0,
+  //                 "align": "center",
+  //                 "weight": "bold"
+  //               },
+  //               {
+  //                 "type": "text",
+  //                 "text": "useridline(username) @(line)",
+  //                 "margin": "sm",
+  //                 "align": "start",
+  //                 "wrap": true
+  //               }
+  //             ]
+  //           },
+  //           {
+  //             "type": "box",
+  //             "layout": "horizontal",
+  //             "contents": [
+  //               {
+  //                 "type": "text",
+  //                 "text": "2.-",
+  //                 "flex": 0,
+  //                 "weight": "bold"
+  //               },
+  //               {
+  //                 "type": "text",
+  //                 "text": "idformDB(idline) @'.$id.' ",
+  //                 "margin": "sm",
+  //                 "align": "start",
+  //                 "wrap": true
+  //               }
+  //             ]
+  //           }
+  //         ]
+  //       },
+  //       "footer": {
+  //         "type": "box",
+  //         "layout": "horizontal",
+  //         "flex": 7,
+  //         "spacing": "sm",
+  //         "margin": "xs",
+  //         "contents": [
+  //           {
+  //             "type": "box",
+  //             "layout": "vertical",
+  //             "flex": 0,
+  //             "contents": [
+  //               {
+  //                 "type": "spacer"
+  //               }
+  //             ]
+  //           },
+  //           {
+  //             "type": "box",
+  //             "layout": "vertical",
+  //             "flex": 0,
+  //             "contents": [
+  //               {
+  //                 "type": "spacer",
+  //                 "size": "xs"
+  //               }
+  //             ]
+  //           },
+  //           {
+  //             "type": "box",
+  //             "layout": "vertical",
+  //             "contents": [
+  //               {
+  //                 "type": "spacer"
+  //               }
+  //             ]
+  //           },
+  //           {
+  //             "type": "box",
+  //             "layout": "vertical",
+  //             "flex": 0,
+  //             "contents": [
+  //               {
+  //                 "type": "button",
+  //                 "action": {
+  //                   "type": "uri",
+  //                   "label": "Manage",
+  //                   "uri": "https://it-not-support.herokuapp.com/user_table.php?id=1"
+  //                 },
+  //                 "color": "#E8F3FF",
+  //                 "margin": "sm",
+  //                 "height": "sm",
+  //                 "style": "secondary"
+  //               }
+  //             ]
+  //           }
+  //         ]
+  //       },
+  //       "styles": {
+  //         "header": {
+  //           "backgroundColor": "#2CA9FF"
+  //         }
+  //       }
+  //     }
+  //   }]';
+  $json_array=[
+        "type" => "template",
+        "altText" => "this is a buttons template",
+        "template" => [
+          "type" => "buttons",
+          "actions" => [
+            [
+              "type" => "message",
+              "label" => "PM 6:00",
+              "text" => "PM 6:00"
+            ],
+            [
+              "type" => "message",
+              "label" => "PM 7:00",
+              "text" => "PM 7:00"
+          ],
+            [
+              "type" => "message",
+              "label" => "PM 8:00",
+              "text" => "PM 8:00"
+          ],
+            [
+              "type" => "message",
+              "label" => "PM 9:00",
+              "text" => "PM 9:00"
+          ]
+          ],
+          "title" => "Select the Reservation time",
+          "text" => "Last Order  => PM 9:00"
+            ]
+        ];
+    // $jsonFlex=json_decode($json_array, true);
+    print_r($json_array);
     if ($message == "test") {
         if (sizeof($arrayJson['events']) > 0) {
             foreach ($arrayJson['events'] as $event) {
@@ -25,194 +254,111 @@ $message = $arrayJson['events'][0]['message']['text'];
                 'replyToken' => $reply_token,
                 'messages' =>[$json_array],
             ];
+                print_r($data);
                 $API_URL = 'https://api.line.me/v2/bot/message';
                 $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
                 $send_result = send_reply_message($API_URL.'/reply', $arrayHeader, $post_body);
                 echo "Result: ".$send_result."\r\n";
             }
         }
+        print_r($post_body);
         print "file ok";
     }
-?>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  
-  <script>
-$(document).ready(function () {
-let json={
-  "type": "flex",
-  "altText": "Flex Message",
-  "contents": {
-    "type": "bubble",
-    "direction": "ltr",
-    "header": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        {
-          "type": "text",
-          "text": "กรรมการรับเศษ(รอบเช้า)",
-          "size": "lg",
-          "align": "center",
-          "gravity": "top",
-          "weight": "bold",
-          "color": "#FFFFFF",
-          "wrap": true
-        }
-      ]
-    },
-    "body": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        {
-          "type": "box",
-          "layout": "horizontal",
-          "contents": [
-            {
-              "type": "text",
-              "text": "1.-",
-              "flex": 0,
-              "align": "center",
-              "weight": "bold"
-            },
-            {
-              "type": "text",
-              "text": "(username 1) (id-line)",
-              "margin": "sm",
-              "align": "start",
-              "wrap": true
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "horizontal",
-          "contents": [
-            {
-              "type": "text",
-              "text": "2.-",
-              "flex": 0,
-              "weight": "bold"
-            },
-            {
-              "type": "text",
-              "text": "(username 2) (id-line)",
-              "margin": "sm",
-              "align": "start",
-              "wrap": true
-            }
-          ]
-        }
-      ]
-    },
-    "footer": {
-      "type": "box",
-      "layout": "horizontal",
-      "flex": 7,
-      "spacing": "sm",
-      "margin": "xs",
-      "contents": [
-        {
-          "type": "box",
-          "layout": "vertical",
-          "flex": 0,
-          "contents": [
-            {
-              "type": "spacer"
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "vertical",
-          "flex": 0,
-          "contents": [
-            {
-              "type": "spacer",
-              "size": "xs"
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "spacer"
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "horizontal",
-          "flex": 10,
-          "margin": "sm",
-          "contents": [
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "Manage",
-                "uri": "line://app/101"
-              },
-              "flex": 0,
-              "color": "#E8F3FF",
-              "margin": "sm",
-              "height": "sm",
-              "style": "secondary"
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "postback",
-                "label": "ok",
-                "text": "ok",
-                "data": "TRUE"
-              },
-              "flex": 0,
-              "color": "#2090FD",
-              "margin": "sm",
-              "height": "sm",
-              "style": "primary"
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "postback",
-                "label": "เลื่อน",
-                "text": "pass",
-                "data": "false"
-              },
-              "flex": 0,
-              "color": "#FF3535",
-              "margin": "xs",
-              "height": "sm",
-              "style": "primary"
-            }
-          ]
-        }
-      ]
-    },
-    "styles": {
-      "header": {
-        "backgroundColor": "#00416E"
-      }
-    }
-  }
-};
 
-let js_data = JSON.stringify(json);
-    $.ajax({
-        type: "post",
-        url: "php/response.php",
-        data: {js_data},
-        success: function (response) {
-            console.log('ok');
-        }
-    });
-});
-  </script>
-  </body>
+    function send_reply_message($url, $post_header, $post_body)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+//
+
+
+
+
+
+
+
+
+    #ตัวอย่าง Message Type "Image"
+    if ($message == "รูปน้องแมว") {
+        $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "image";
+        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
+        $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
+        replyMsg($arrayHeader, $arrayPostData);
+    }
+    #ตัวอย่าง Message Type "Location"
+    elseif ($message == "พิกัดสยามพารากอน") {
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "location";
+        $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
+        $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
+        $arrayPostData['messages'][0]['latitude'] = "13.7465354";
+        $arrayPostData['messages'][0]['longitude'] = "100.532752";
+        replyMsg($arrayHeader, $arrayPostData);
+    }
+    #ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"
+    elseif ($message == "ลาก่อน") {
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
+        $arrayPostData['messages'][1]['type'] = "sticker";
+        $arrayPostData['messages'][1]['packageId'] = "1";
+        $arrayPostData['messages'][1]['stickerId'] = "131";
+        replyMsg($arrayHeader, $arrayPostData);
+
+        // ฟังชั่น ตอบกลับ
+    }
+    function replyMsg($arrayHeader, $arrayPostData)
+    {
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }   exit;
+// push message
+function pushMsg($arrayHeader, $arrayPostData)
+{
+    $strUrl = "https://api.line.me/v2/bot/message/push";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}exit;
+
+// log read id_usert
+
+$json = file_get_contents('php://input');
+$request = json_decode($json, true);
+$queryText = $request["queryResult"]["queryText"];
+$userId = $request['originalDetectIntentRequest']['payload']['data']['source']['userId'];
+$myfile = fopen("textfile.txt", "a") or die("Unable to open file!");
+$log = $date."-".$time."\t".$userId."\t".$queryText."\n";
+fwrite($myfile,$log);
+fclose($myfile);
+// end log read id_user
+
+?>
+</body>
 </html>
