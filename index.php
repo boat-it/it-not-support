42b3f64bba1502f25a9b341a345243f6d985cd10
it-support <botzacabal16@gmail.com>
2020-01-16 16:01:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,30 @@
   <body>
 <?php
 include 'connectstring.php'; // postgres connect
-
+$accessToken = "kT9H2mrXWPMGeaTwwUpqu3RXRTTghlSAHXaPk+jZWC7kW8lI9pkbi8po6wemhLv3wzp7FUnh52sTOYbu+b1pPWMTIkGuqEuKAG2h3oqHFtkc23sSukoDHo6+o2e64a01J00m0JVo4h4wM2jDD+r2bQdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+$channelSecret = '0e14c3f144f47fbc2d247184253c0bf6';
+$content = file_get_contents('php://input');
+$arrayJson = json_decode($content, true);
+$message = $arrayJson['events'][0]['message']['text'];    
+    $json_array = json_decode($_POST['json']);
+    if ($message == "test") {
+        if (sizeof($arrayJson['events']) > 0) {
+            foreach ($arrayJson['events'] as $event) {
+                error_log(json_encode($event));
+                $reply_message = '';
+                $reply_token = $event['replyToken'];
+                $data = [
+                'replyToken' => $reply_token,
+                'messages' =>[$json_array],
+            ];
+                $API_URL = 'https://api.line.me/v2/bot/message';
+                $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+                $send_result = send_reply_message($API_URL.'/reply', $arrayHeader, $post_body);
+                echo "Result: ".$send_result."\r\n";
+            }
+        }
+        print "file ok";
+    }
 ?>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
