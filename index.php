3703f5edaf032165ae27233ff23fc5f776044ccd
it-support <botzacabal16@gmail.com>
2020-01-16 15:56:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,195 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
 <?php
-include 'callAPI.php';
-include 'connectstring.php';
-    $accessToken = "kT9H2mrXWPMGeaTwwUpqu3RXRTTghlSAHXaPk+jZWC7kW8lI9pkbi8po6wemhLv3wzp7FUnh52sTOYbu+b1pPWMTIkGuqEuKAG2h3oqHFtkc23sSukoDHo6+o2e64a01J00m0JVo4h4wM2jDD+r2bQdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
-    $channelSecret = '0e14c3f144f47fbc2d247184253c0bf6';
-    $content = file_get_contents('php://input');
-   $arrayJson = json_decode($content, true);
-   $arrayHeader = array();
-   $arrayHeader[] = "Content-Type: application/json";
-   $arrayHeader[] = "Authorization: Bearer {$accessToken}";//รับข้อความจากผู้ใช้
-   $message = $arrayJson['events'][0]['message']['text'];//รับ id ว่ามาจากไหน
-   $j=$arrayJson['events'][0]['source'];
-   if(isset($arrayJson['events'][0]['source']['userId'])){
-      $id = $arrayJson['events'][0]['source']['userId'];
-      $id2 = $arrayJson['events'][0]['source']['userId']['user'];
-   }
-   else if(isset($arrayJson['events'][0]['source']['groupId'])){
-      $id = $arrayJson['events'][0]['source']['groupId'];
-   }
-   else if(isset($arrayJson['events'][0]['source']['room'])){
-      $id = $arrayJson['events'][0]['source']['room'];
-   }
-   if($message == "ID"){
-      $arrayPostData['to'] = $id;
-      $arrayPostData['messages'][0]['type'] = "text";
-      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
-      $arrayPostData['messages'][1]['type'] = "sticker";
-      $arrayPostData['messages'][1]['packageId'] = "2";
-      $arrayPostData['messages'][1]['stickerId'] = "34";
-      $arrayPostData['messages'][2]['type'] = 'text';
-      $arrayPostData['messages'][2]['text'] = "you Id ".var_dump($j).": [$id]:[$id2]";
-      try {
-          $sql="UPDATE user_table set id_line='$id' where username='yodsapon'";
-          $stmt=$dbh->query($sql);
-      }catch(Exception $e){
-          echo $e->getMessage(),$e->getLine();
-      }
-      pushMsg($arrayHeader,$arrayPostData);
-   }
+include 'connectstring.php'; // postgres connect
 
-
-
-
-print 'ok';
-
-    function pushMsg($arrayHeader,$arrayPostData){
-      $strUrl = "https://api.line.me/v2/bot/message/push";
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL,$strUrl);
-      curl_setopt($ch, CURLOPT_HEADER, false);
-      curl_setopt($ch, CURLOPT_POST, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      $result = curl_exec($ch);
-      curl_close ($ch);
-   }exit;
 ?>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
+  <script>
+$(document).ready(function () {
+let json={
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "bubble",
+    "direction": "ltr",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "กรรมการรับเศษ(รอบเช้า)",
+          "size": "lg",
+          "align": "center",
+          "gravity": "top",
+          "weight": "bold",
+          "color": "#FFFFFF",
+          "wrap": true
+        }
+      ]
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "text",
+              "text": "1.-",
+              "flex": 0,
+              "align": "center",
+              "weight": "bold"
+            },
+            {
+              "type": "text",
+              "text": "(username 1) (id-line)",
+              "margin": "sm",
+              "align": "start",
+              "wrap": true
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "contents": [
+            {
+              "type": "text",
+              "text": "2.-",
+              "flex": 0,
+              "weight": "bold"
+            },
+            {
+              "type": "text",
+              "text": "(username 2) (id-line)",
+              "margin": "sm",
+              "align": "start",
+              "wrap": true
+            }
+          ]
+        }
+      ]
+    },
+    "footer": {
+      "type": "box",
+      "layout": "horizontal",
+      "flex": 7,
+      "spacing": "sm",
+      "margin": "xs",
+      "contents": [
+        {
+          "type": "box",
+          "layout": "vertical",
+          "flex": 0,
+          "contents": [
+            {
+              "type": "spacer"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "flex": 0,
+          "contents": [
+            {
+              "type": "spacer",
+              "size": "xs"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "spacer"
+            }
+          ]
+        },
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "flex": 10,
+          "margin": "sm",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "Manage",
+                "uri": "line://app/101"
+              },
+              "flex": 0,
+              "color": "#E8F3FF",
+              "margin": "sm",
+              "height": "sm",
+              "style": "secondary"
+            },
+            {
+              "type": "button",
+              "action": {
+                "type": "postback",
+                "label": "ok",
+                "text": "ok",
+                "data": "TRUE"
+              },
+              "flex": 0,
+              "color": "#2090FD",
+              "margin": "sm",
+              "height": "sm",
+              "style": "primary"
+            },
+            {
+              "type": "button",
+              "action": {
+                "type": "postback",
+                "label": "เลื่อน",
+                "text": "pass",
+                "data": "false"
+              },
+              "flex": 0,
+              "color": "#FF3535",
+              "margin": "xs",
+              "height": "sm",
+              "style": "primary"
+            }
+          ]
+        }
+      ]
+    },
+    "styles": {
+      "header": {
+        "backgroundColor": "#00416E"
+      }
+    }
+  }
+};
+
+let js_data = JSON.stringify(json);
+    $.ajax({
+        type: "post",
+        url: "php/response.php",
+        data: {js_data},
+        success: function (response) {
+            console.log('ok');
+        }
+    });
+});
+  </script>
+  </body>
+</html>
